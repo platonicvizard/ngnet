7e0287e9690303ab9b84fd15f2fd805f18f1d48a
Lisandro Martinez <lmartinezm@msn.com>
2017-07-04 00:17:56 -0400

    first commit

---
+++ dotnetWeb/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace dotnetWeb.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.IO;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var fileName = "wwwroot\\index.html";
+            var contentType = "text/html";
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            string fileContents = System.IO.File.ReadAllText(filePath);
+
+            return Content(fileContents, contentType);
+        }
+
+        public IActionResult Error()
+        {
+            return ActionResult();
+        }
+        }
+}
